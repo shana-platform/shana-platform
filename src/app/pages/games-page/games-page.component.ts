32e33417ab9b54b427cfbdcb15561e70bcea797c
marian.boye-amoah <mboye-amoah@itconsortiumgh.com>
2025-09-02 13:37:35 +0000

    - pushing updates for module 5

--- src/app/pages/games-page/games-page.component.ts
+++ src/app/pages/games-page/games-page.component.ts
@@ -10,7 +10,8 @@ export class GamesPageComponent implements OnInit {
     { name: 'Kahoot - Using My Strengths', link: '/second-week-level5'  },
     { name: 'My Hero Me', link: '/third-week-level5'  },
     { name: 'Wheel Of Names', link: 'https://wheelofnames.com/5py-vhj'  },
-    { name: 'This_or_That_Me_Game', link: 'https://docs.google.com/presentation/d/1Y3d73Dlrcf-seAgVRZgfm5Vz5scWoctI/edit?slide=id.p11#slide=id.p11'  }
+    { name: 'This_or_That_Me_Game', link: 'https://docs.google.com/presentation/d/1Y3d73Dlrcf-seAgVRZgfm5Vz5scWoctI/edit?slide=id.p11#slide=id.p11'  },
+    { name: 'Proud Star Toss', link: '/fifth-week-level5'  }
   ];
   constructor() { }
 
