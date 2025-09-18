286b28b3be5c3cde5f357a67d5333a715b3987a0
Jude Asiedu <jude.asiedu99@gmail.com>
2025-09-18 18:15:26 +0000

    Updated modules

--- src/app/pages/users/simplepath/simplepath.module.ts
+++ src/app/pages/users/simplepath/simplepath.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WeeklydetailsComponent } from './weeklydetails/weeklydetails.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WeeklydetailsComponent
+  ],
   imports: [
     CommonModule
   ]
--- src/app/pages/users/users-routing.module.ts
+++ src/app/pages/users/users-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { WeeklydetailsComponent } from './simplepath/weeklydetails/weeklydetails.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {path:'dashboard',component:DashboardComponent},
+  {path:'simple',children:[
+    {path:'weekly/:id', component:WeeklydetailsComponent},
+    {path:'lesson/', component:WeeklydetailsComponent},
+    {path:'games/', component:WeeklydetailsComponent}
+  ]},
+  {path:'advanced',children:[
+
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
