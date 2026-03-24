491713a74edbbf498e92993c89ef8dcbe0d4105f
Marian-Amoah <marianboyeamoah@gmail.com>
2026-03-24 20:39:56 +0000

    - pusing updates to production

--- src/app/core/services/student.service.ts
+++ src/app/core/services/student.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
 
-  private baseUrl = 'http://localhost:3000/api/students';
+  private baseUrl = `${environment.apiUrl}/students`;
 
   constructor(private http: HttpClient) {}
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api-shana-beginners.onrender.com/api'
 };
