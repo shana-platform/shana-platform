b19755c3dbfdb34edfac10f4291dd8d0e2ea3080
marian.boye-amoah <mboye-amoah@itconsortiumgh.com>
2026-03-26 08:39:50 +0000

    Added Netlify environment config

--- set-env.js
+++ set-env.js
@@ -1,15 +1,12 @@
 const fs = require('fs');
 
+const apiUrl = process.env.API_URL || 'http://localhost:3000/api';
+
 const envConfig = `
 window.__env = {
-  API_URL: "${process.env.API_URL}"
+  API_URL: "${apiUrl}"
 };
 `;
 
-fs.writeFile('./src/assets/env.js', envConfig, err => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log('Environment file created!');
-});
+fs.writeFileSync('./src/assets/env.js', envConfig);
+console.log('env.js created with API_URL:', apiUrl);
