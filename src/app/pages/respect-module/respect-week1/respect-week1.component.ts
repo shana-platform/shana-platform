b152eab0fd7afd1d076ac7c1a320ad83d71459f5
marian.boye-amoah <mboye-amoah@itconsortiumgh.com>
2026-03-31 20:03:22 +0000

    pushing updates for bug fixes and UI Enhancements

--- src/app/pages/respect-module/respect-week1/respect-week1.component.ts
+++ src/app/pages/respect-module/respect-week1/respect-week1.component.ts
@@ -62,14 +62,13 @@ export class RespectWeek1Component implements OnInit {
 
   ngOnInit(): void {
     this.progressService.currentProgress$.pipe(take(1)).subscribe(progress => {
-      console.log(progress)
       if (progress['week1'] == 30) {
         alert('You have already completed Level 1! Taking you to Level 2.');
         this.router.navigate(['/respect-week1-l1']);
       } else if (progress['week1'] == 70) {
         alert('You have already completed Level 2! Taking you to Level 3.');
         this.router.navigate(['/respect-week1-l2']);
-      } else {
+      } else if (progress['week1'] == 100){
         alert('You have already completed week1! Taking you to week 2');
         this.router.navigate(['/respect-week2']);
       }
